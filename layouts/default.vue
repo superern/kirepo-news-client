6ac566deee54a090f99d86bd092a175ec7ff53ac
Superern <ernandrewgregorio@gmail.com>
2021-07-22 14:51:05 +0800

    add home, new, tag pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,13 @@
 <template>
   <div>
     <nav
-      class="navbar header has-shadow is-primary"
+      class="navbar header has-shadow"
       role="navigation"
       aria-label="main navigation"
     >
       <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
-          >
+        <a class="navbar-item" href="/">
+          <img src="~assets/images/logo.svg" alt="Buefy" height="28" />
         </a>
 
         <div class="navbar-burger">
@@ -25,20 +18,13 @@
       </div>
     </nav>
 
+    <!-- Main Content -->
     <section class="main-content columns">
       <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
+        <p class="menu-label is-hidden-touch">General</p>
         <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <NuxtLink
-              :to="item.to"
-              exact-active-class="is-active"
-            >
+          <li v-for="(item, key) of items" :key="key">
+            <NuxtLink :to="item.to" exact-active-class="is-active">
               <b-icon :icon="item.icon" /> {{ item.title }}
             </NuxtLink>
           </li>
@@ -54,21 +40,32 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       items: [
         {
           title: 'Home',
           icon: 'home',
-          to: { name: 'index' }
+          to: { name: 'index' },
         },
         {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
+          title: 'News',
+          icon: 'newspaper',
+          to: { name: 'news' },
+        },
+        {
+          title: 'Tags',
+          icon: 'tag',
+          to: { name: 'tags' },
+        },
+      ],
     }
-  }
+  },
 }
 </script>
+
+<style scoped>
+.main-content {
+  background: #f5f5f5;
+}
+</style>
